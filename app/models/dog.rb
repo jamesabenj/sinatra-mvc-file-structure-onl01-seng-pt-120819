92913cafe835acbd31e4b1c04425fc6b62295422
James Benjamin <james.abenj@gmail.com>
2020-03-09 17:55:03 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,17 @@
-class Dog < Sinatra::Base
+class Dog 
+  attr_accessor :name, :breed, :age 
 
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age 
+    @@all << self
+  end 
+
+  def self.all 
+    @@all
+  end 
   
 end 
